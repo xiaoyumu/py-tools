754286e9f148f1664665ff0be6b5ef84d18d49e8
xiaoyumu <xiaoyumu@gmail.com>
2024-09-13 18:44:02 +0800

    Update help text

--- hf-dl.py
+++ hf-dl.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from urllib.parse import urlparse, unquote
 
 
-def download_file(url, output_folder, overwrite=False):
+def download_file(url, output_folder, overwrite=False, http_proxy=None):
     # Check if the output path is a file
     if os.path.isfile(output_folder):
         print(f"Error: The specified output path '{output_folder}' is a file, not a folder.")
@@ -26,10 +26,16 @@ def download_file(url, output_folder, overwrite=False):
         print(f"Error: File '{file_path}' already exists. Use --overwrite to overwrite it.")
         return
     print(f"Downloading {filename}:")
+
+    proxies = {
+        'http': http_proxy,
+        'https': http_proxy,
+    } if http_proxy else None
+
     # Download the file with progress bar
-    response = requests.get(url, stream=True)
+    response = requests.get(url, stream=True, proxies=proxies)
     total_size = int(response.headers.get('content-length', 0))
-    block_size = 1024  # 1 Kibibyte
+    block_size = 1024  # 1 Kibi byte
     progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
 
     with open(file_path, 'wb') as file:
@@ -45,11 +51,16 @@ def download_file(url, output_folder, overwrite=False):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download huggingface model from URL to local folder.")
+
+    parser = argparse.ArgumentParser(description="Download huggingface model from URL to local folder.",
+                                     )
     parser.add_argument("--url", type=str, required=True, help="URL of the file to download.")
     parser.add_argument("--output", type=str, required=True, help="Path to the output folder.")
     parser.add_argument("--overwrite", action="store_true", help="Overwrite existing file if it exists.")
+    parser.add_argument("--proxy", type=str, required=False,
+                        help="Http proxy, set to use proxy to download files. Support socks5 proxies. "
+                             "Use socks5h:// to enable host resolving with socks5 proxy.")
 
     args = parser.parse_args()
 
-    download_file(args.url, args.output, args.overwrite)
+    download_file(args.url, args.output, args.overwrite, args.proxy)
